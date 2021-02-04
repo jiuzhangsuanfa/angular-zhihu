b822d4a261ef19ebcb2991505e54238eeb8c3440
Devin Don <i.inf@outlook.com>
2021-02-04 12:44:11 +0800

    feat(answer/detail): add action button

--- src/app/answer/components/detail/answer-detail.component.ts
+++ src/app/answer/components/detail/answer-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Answer, AnswerID } from 'src/app/common/interfaces';
+import { AnswerApiService } from '../../services/api/answer-api.service';
 
 @Component({
   selector: 'app-answer-detail',
@@ -7,8 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnswerDetailComponent implements OnInit {
 
-  constructor() { }
+  id: AnswerID;
+  answer?: Answer;
 
-  ngOnInit() { }
+  constructor(
+    private api: AnswerApiService,
+    private route: ActivatedRoute,
+  ) {
+    this.id = +route.snapshot.paramMap.get('id')!;
+  }
+
+  ngOnInit() {
+    this.api.getAnswer(this.id)
+      .subscribe(answer => this.answer = answer);
+  }
 
 }
