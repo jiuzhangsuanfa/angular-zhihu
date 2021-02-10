d2e8ccd1685c4c44fba7e3afd7122195fad2a9cf
Devin Don <i.inf@outlook.com>
2021-02-10 09:43:49 +0800

    chore(prod): optimize prod mode

--- src/app/common/constants/index.ts
+++ src/app/common/constants/index.ts
@@ -1,5 +1,6 @@
 import { isDevMode } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 export const HOST = isDevMode()
   ? 'http://localhost:8080'
-  : 'http://server';
+  : environment.host;
